e001cefabdbe8d6a390301bf48b0b83efce3c8d9
Hashim017 <120157540+Hashim017@users.noreply.github.com>
2022-12-20 08:27:20 -0800

    pf-week2

---
+++ task9.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+main(){
+
+cout <<"                   ------------------------------------        " <<endl;
+cout <<"                                                               " <<endl;
+cout <<"                      o   ^____^                               " <<endl;
+cout <<"                       o   (oo)|___________    /               " <<endl;
+cout <<"                           (__)/           )__/                " <<endl;
+cout <<"                               ||         |                    " <<endl;
+cout <<"                               ||-------L |                    " <<endl;
+cout <<"                               ||         ||                   " <<endl;
+
+}
